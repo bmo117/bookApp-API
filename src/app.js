ae7f38150b6c6a06ecf9a3f55ac0b78bd02ea4ee
BMO <brandon0891996@gmail.com>
2023-06-15 02:43:45 -0600

    cors all accessv8

--- src/app.js
+++ src/app.js
@@ -12,13 +12,8 @@ import { PORT } from "../src/config.js";
 //config();
 const app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
